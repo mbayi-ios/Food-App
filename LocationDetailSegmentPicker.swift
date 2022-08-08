24e846c7178774ebe7ca967e21fac48a3b0cf805
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-08 22:07:06 +0300

    LocationDetails
    - worked on location detail segment picker

--- LocationDetailSegmentPicker.swift
+++ LocationDetailSegmentPicker.swift
@@ -7,14 +7,72 @@
 
 import SwiftUI
 
+enum LocationDetailSegments {
+    case left
+    case mid
+    case right
+}
+
 struct LocationDetailSegmentPicker: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @Environment(\.theme) var theme: Theme
+
+    private let leftTitle: String
+    private let middleTitle: String
+    private let rightTitle: String
+
+    @Binding var selected: LocationDetailSegments
+
+    @Namespace private var animation
+
+    init(leftTitle: String, middleTitle: String, rightTitle: String, selected: Binding<LocationDetailSegments>) {
+        self.leftTitle = leftTitle
+        self.middleTitle = middleTitle
+        self.rightTitle = rightTitle
+        self._selected = selected
     }
+
+    var body: some View {
+        VStack  {
+            HStack {
+                makeSegmentButton(title: leftTitle, segment: .left)
+                makeSegmentButton(title: middleTitle, segment: .mid)
+                makeSegmentButton(title: rightTitle, segment: .right)
+            }
+            Divider()
+        }
+        .background(theme.colors.textOnPrimary)
+    }
+
+    private func makeSegmentButton(title: String, segment: LocationDetailSegments) -> some View {
+        Button {
+            withAnimation(.interactiveSpring()) {
+                selected = segment
+            }
+        } label: {
+            VStack {
+                Text(title)
+                    .frame(maxWidth: .infinity, minHeight: 34)
+                    .font(theme.typography.body)
+                    .foregroundColor(selected == segment ? theme.colors.textDefault : theme.colors.textSubdued)
+            }
+        }
+        .background(
+            ZStack {
+                if selected == segment {
+                    theme.colors.actionPrimary
+                        .matchedGeometryEffect(id: "Tab", in: animation)
+                        .offset(y: 22.5)
+                        .frame(width: .infinity, height: 5, alignment: .center)
+                }
+            }
+        )
+    }
+
 }
 
 struct LocationDetailSegmentPicker_Previews: PreviewProvider {
+    @State static var selectedSegment: LocationDetailSegments = .left
     static var previews: some View {
-        LocationDetailSegmentPicker()
+        LocationDetailSegmentPicker(leftTitle: "Store Hours", middleTitle: "Delivery Hours", rightTitle: "Pickup Hourse", selected: $selectedSegment)
     }
 }
--- LocationDetailView.swift
+++ LocationDetailView.swift
@@ -15,16 +15,85 @@ struct LocationDetailView: View {
     private let location: Location
     private let dayKeys = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40.73610, longitude: -73.935242), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -1.286389, longitude: 36.817223), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
 
-    //@State private var selectedSegment: LocationDetailSegments = .left
+    @State private var selectedSegment: LocationDetailSegments = .left
 
     init(location: Location) {
         self.location = location
     }
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            ZStack {
+                closeButton()
+                    .padding()
+                    .frame(maxWidth: .infinity, maxHeight: 220, alignment: .leading)
+            }.background(
+                Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+            )
+            VStack(alignment: .leading, spacing: 16){
+                VStack(alignment: .leading) {
+                    textLabel(location.name, theme.typography.displayMedium, theme.colors.textDefault)
+
+                    textLabel(location.isOpen ? "Open Now" : "Closed Now", theme.typography.caption, location.isOpen ? theme.colors.actionPrimary : theme.colors.textCritical)
+                }
+
+                HStack {
+                    Image("PhoneIcon", bundle: .main).renderingMode(.template)
+                    Button(location.address) { print("location link")}.link()
+                }
+            }.padding()
+
+            LocationDetailSegmentPicker(leftTitle: "Store Hours", middleTitle: "Delivery Hours", rightTitle: "Pickup Hours", selected: $selectedSegment)
+            bodySection()
+            Spacer()
+        }
+    }
+    private func bodySection() -> some View {
+        Group {
+            switch selectedSegment {
+            case .left, .right:
+                displayHours(selectedHours: location.pickupHours)
+
+            case .mid:
+                displayHours(selectedHours: location.deliveryHours)
+            }
+        }
+    }
+
+    private func displayHours(selectedHours: [String: String]) -> some View {
+        VStack {
+            ForEach(dayKeys, id: \.self) { day in
+                GeometryReader { geometry  in
+                    HStack {
+                        Text(day)
+                            .frame(width: geometry.size.width / 3, alignment: .leading)
+                        Text(selectedHours[day] ?? "Closed")
+                    }.font(theme.typography.body)
+                }
+            }.frame(height: 25)
+        }.padding()
+    }
+
+    private func closeButton() -> some View {
+        Button {
+            dismiss()
+        } label: {
+            ZStack {
+                Circle()
+                    .fill(theme.colors.textOnPrimary)
+                    .frame(width: 44, height: 44)
+                Image(systemName: "xmark")
+                    .font(theme.typography.headingMedium)
+                    .tint(.black)
+            }
+        }
+    }
+
+    fileprivate func textLabel(_ text: String, _ font: Font, _ color: Color) -> Text {
+        return Text(text).font(font)
+            .foregroundColor(color)
     }
 }
 
